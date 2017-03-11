bed4d9d57c2db144a9dbbb8229d71ef2dea4247a
SheilaOM <sheila.oliva14@gmail.com>
2017-03-11 14:10:09 +0100

    Práctica terminada. Primera versión

--- practica1.py
+++ practica1.py
@@ -7,6 +7,7 @@ import csv
 class contentApp (webapp.webApp):
 
     list_urls = {}
+    list_espj = {}
 
     def inicializa(self):
         import os.path
@@ -15,7 +16,8 @@ class contentApp (webapp.webApp):
                 read = csv.reader(fich, delimiter=',')
                 for row in read:
                     self.list_urls[row[0]] = row[1]
-            self.numurl = int(row[0])
+                    self.list_espj[row[1]] = row[0]
+            self.numurl = len(self.list_urls)
         else:
             self.numurl = 0
 
@@ -24,7 +26,6 @@ class contentApp (webapp.webApp):
             salida = csv.writer(fich, delimiter=',')
             for element in self.list_urls:
                 salida.writerow([element, self.list_urls[element]])
-
 
     def parse(self, request):
         self.inicializa()
@@ -42,38 +43,65 @@ class contentApp (webapp.webApp):
     def process(self, parsed):
         metodo, recurso, url_larga = parsed
         if (metodo == "GET"):
-            if (recurso == "/"):
-                if self.numurl == 0:
-                    url_guardadas = "No hay urls guardadas."
-                else:
-                    url_guardadas = "La lista de urls guardadas es:<br>"
-                for element in self.list_urls:
-                    url_guardadas = url_guardadas + (element + " -> " + self.list_urls[element] + "<br>")
+            if recurso == "favicon.ico":
+                httpCode = "HTTP/1.1 404 Not found"
+                httpBody = "<html><body><h1>Not Found</h1></body></html>"
 
-                httpCode = "200 OK"
-                htmlBody = "<html><body>" + url_guardadas + \
-                            "<form method='POST' action=''><input type='text'" \
-                            " name='url_larga'><input type='submit' value='Acortar'></form>" \
-                            + "</body></html>"
+            elif recurso == "/":
+                if self.numurl == 0:
+                    url_guard = "No hay urls guardadas."
+                else:
+                    url_guard = "La lista de urls guardadas es:<br>"
+                for corta, larga in self.list_urls.items():
+                    url_guard = url_guard + (corta + " -> " + larga + "<br>")
+
+                httpCode = "HTTP/1.1 200 OK"
+                htmlBody = "<html><body>" + url_guard +\
+                           "<form method='POST' action=''><input type='text'" \
+                           " name='url_larga'><input type='submit' " \
+                           "value='Acortar'></form></body></html>"
 
             else:
                 num_corta = recurso[1:]
-                url_larga = self.list_urls[num_corta]
-                httpCode = "HTTP/1.1 302 Redirect"
-                htmlBody = "<html><body>Redireccionando a: " + url_larga +\
-                            "<meta http-equiv='Refresh' content='1;url=" + url_larga + "'></body></html>"
+                if num_corta in self.list_urls:
+                    url_larga = self.list_urls[num_corta]
+                    httpCode = "HTTP/1.1 302 Redirect"
+                    htmlBody = "<html><body>Redireccionando a: " + url_larga +\
+                               "<meta http-equiv='Refresh' content='1;url=" +\
+                               url_larga + "'></body></html>"
+                else:
+                    httpCode = "HTTP/1.1 404 Not Found"
+                    htmlBody = "<html><body>Recurso no disponible" +\
+                               "</body></html>"
 
         elif (metodo == "POST"):
             if (recurso == "/"):
-                httpCode = "200 OK"
-                import urllib.parse
-                url_larga = urllib.parse.unquote(url_larga)
-                self.list_urls[str(self.numurl)] = url_larga
-                self.persiste()
-                url_corta = "/" + str(self.numurl)
-                htmlBody = "<html><body>La URL acortada de <a href=" + url_larga + ">" + url_larga +\
-                            "</a> es <a href=" + url_corta + ">http://localhost:1234" + url_corta + "</a></body></html>"
-                self.numurl += 1
+                if url_larga == "":
+                    httpCode = "HTTP/1.1 404 Not Found"
+                    htmlBody = "<html><body>Error. Debes introducir una url." \
+                               "</body></html>"
+                else:
+                    httpCode = "HTTP/1.1 200 OK"
+                    import urllib.parse
+                    url_larga = urllib.parse.unquote(url_larga)
+
+                    if (url_larga[0:7] != "http://" and
+                            url_larga[0:8] != "https://"):
+                        url_larga = "http://" + url_larga
+
+                    if url_larga in self.list_espj:
+                        url_corta = "/" + self.list_espj[url_larga]
+                    else:
+                        self.list_urls[str(self.numurl)] = url_larga
+                        self.persiste()
+                        url_corta = "/" + str(self.numurl)
+
+                    htmlBody = "<html><body>La URL acortada de <a href=" +\
+                               url_larga + ">" + url_larga + "</a> es " +\
+                               "<a href=" + url_corta +\
+                               ">http://localhost:1234" +\
+                               url_corta + "</a></body></html>"
+                    self.numurl += 1
 
         return (httpCode, htmlBody)
 
